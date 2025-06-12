0ffcd99cc55cc6db2806b5ff6463f2cedc13bbb4
David Schinagl <d.schinagl@github.com>
2025-06-12 16:33:35 +0200

    rm neovide

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,17 +2,6 @@
 
 require('options')
 require('keymaps')
+require('plugins')
+require('themes') -- at end to overwrite other plugins colors
 
--- check if in vscode
-if vim.fn.exists('g:vscode') == 0 then
-  require('themes') -- at end to overwrite other plugins colors
-  require('plugins')
-else
-    require('plugins_vscode')
-end
-
-if vim.g.neovide then
-  vim.g.neovide_scale_factor = 0.85
-  vim.g.neovide_refresh_rate = 60
-  vim.g.neovide_cursor_animation_length = 0.03
-end
